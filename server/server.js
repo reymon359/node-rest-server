ef6ad14d6d11ab6d4241486ff9c62b33d5deb1e5
ramon morcillo <ramonmorcillocascales@gmail.com>
2018-12-12 15:42:24 +0100

    Testing CRUD requests

--- server/server.js
+++ server/server.js
@@ -1,10 +1,30 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-app.get('/', function(req, res) {
-    res.json('Hello World')
+const bodyParser = require('body-parser');
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.get('/users', function(req, res) {
+    res.json('get user')
 })
+app.post('/users', function(req, res) {
+    let body = req.body;
 
+    res.json({ body });
+})
+app.put('/users/:id', function(req, res) {
+    let id = req.params.id;
+    res.json({
+        id
+    });
+})
+app.delete('/users', function(req, res) {
+    res.json('delete user')
+})
 app.listen(3000, () => {
     console.log('Listening port: ', 3000);
 });
