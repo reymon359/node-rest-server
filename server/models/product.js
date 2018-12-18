060c64eb6fc8bfafd2ec5d6d9972cbec6b75d868
ramon morcillo <ramonmorcillocascales@gmail.com>
2018-12-18 17:44:06 +0100

    created product model

---
+++ server/models/product.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+
+var productSchema = new Schema({
+    name: { type: String, required: [true, 'The name is required'] },
+    unitPrice: { type: Number, required: [true, 'The unit price is required'] },
+    description: { type: String, required: false },
+    avaliable: { type: Boolean, required: true, default: true },
+    category: { type: Schema.Types.ObjectId, ref: 'Category', required: true },
+    user: { type: Schema.Types.ObjectId, ref: 'User' }
+});
+
+
+module.exports = mongoose.model('Product', productSchema);
