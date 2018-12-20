62615fd03c7e744e189acbc283737a7a32b8838d
ramon morcillo <ramonmorcillocascales@gmail.com>
2018-12-20 15:28:23 +0100

    +2 upload folders for type and id. Updated the request in the upload.js

--- server/routes/upload.js
+++ server/routes/upload.js
@@ -2,10 +2,19 @@ const express = require('express');
 const fileUpload = require('express-fileupload');
 const app = express();
 
+const User = require('../models/user');
+
 // default options
 app.use(fileUpload());
 // When we call app.use(fileUpload());  all the files go to req.files
-app.post('/upload', function(req, res) {
+
+
+// app.post('/upload', function(req, res) { // The old way
+app.put('/upload/:type/:id', function(req, res) {
+
+    let type = req.params.type;
+    let id = req.params.id;
+
     if (Object.keys(req.files).length == 0) {
         return res.status(400).json({
             ok: false,
@@ -14,6 +23,20 @@ app.post('/upload', function(req, res) {
             }
         });
     }
+
+    // Type validation
+    let validTypes = ['products', 'users'];
+    if (validTypes.indexOf(type) < 0) {
+        return res.status(400).json({
+            ok: false,
+            err: {
+                message: 'The types allowed are: ' + validTypes.join(', '),
+                type
+            }
+        });
+    }
+
+
     let file = req.files.file;
     let fileNameSplited = file.name.split('.');
     let extension = fileNameSplited[fileNameSplited.length - 1];
@@ -37,7 +60,10 @@ app.post('/upload', function(req, res) {
         });
     }
 
-    file.mv(`uploads/${file.name}`, (err) => {
+    // Change the name of the file
+    let fileName = `${id}-${new Date().getMilliseconds()}.${extension}`;
+
+    file.mv(`uploads/${type}/${fileName}`, (err) => {
         if (err)
             return res.status(500).json({
                 ok: false,
